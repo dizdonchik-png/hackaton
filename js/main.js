9a97c0c2616fb9ec15b1e86e3dba7f5f95b14e9e
dizdonchik-png <56499507+dizdonchik-png@users.noreply.github.com>
2020-05-01 12:14:30 +0300

    Создание проекта#

---
+++ js/main.js
@@ -0,0 +1,37 @@
+(function($) {
+
+	"use strict";
+
+	/*---------------------------------------------------- */
+	/* Preloader
+	------------------------------------------------------ */ 
+   $(window).load(function() {
+    	$("#loader").fadeOut("slow", function(){
+        $("#preloader").delay(300).fadeOut("slow");
+
+      });       
+
+  	})
+
+
+
+
+	/*----------------------------------------------------*/
+  	/* Smooth Scrolling
+  	------------------------------------------------------*/
+  	$('.smoothscroll').on('click', function (e) {
+	 	
+	 	e.preventDefault();
+
+   	var target = this.hash,
+    	$target = $(target);
+
+    	$('html, body').stop().animate({
+       	'scrollTop': $target.offset().top
+      }, 800, 'swing', function () {
+      	window.location.hash = target;
+      });
+
+  	});  	
+
+})(jQuery);
